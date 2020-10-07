a2059a430745359a6f73a073cc5aea150758b6b1
Jefferson <jeffersonbraster@gmail.com>
2020-10-06 22:47:51 -0300

    Criando componente de loading e message

--- front/src/screens/HomeScreen.js
+++ front/src/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import {Row, Col} from 'react-bootstrap'
 import {listProducts} from '../actions/productActions'
 
@@ -17,7 +19,7 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Lista de produtos</h1>
-      {loading ? <h2>Carregando...</h2> : error ? <h3>{error}</h3> : (
+      {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
          <Row>
          {products.map(product => (
            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
