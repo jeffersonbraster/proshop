157eb6558adabc7dee7a1e0100a2069e095b6d26
Jefferson <jeffersonbraster@gmail.com>
2020-10-07 12:29:55 -0300

    Fazendo funcionalidade de adicionar ao carrinho

--- front/src/screens/ProductScreen.js
+++ front/src/screens/ProductScreen.js
@@ -9,7 +9,7 @@ import {listProductDetails} from '../actions/productActions'
 
 const ProductScreen = ({history, match}) => {
 
-  const [qty, setQty] = useState(0)
+  const [qty, setQty] = useState(1)
 
   const dispatch = useDispatch()
 
@@ -21,7 +21,7 @@ const ProductScreen = ({history, match}) => {
   }, [dispatch, match])
   
   const addToCartHandler = () => {
-    history.push(`cart/${match.params.id}?qty=${qty}`)
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
   }
   
 
