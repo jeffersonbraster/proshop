ee7507004b69d50cfa50733dd23bc6a95d81ca07
Jefferson <jeffersonbraster@gmail.com>
2020-10-09 12:40:28 -0300

    Criando metodo de pagament

--- front/src/components/CheckoutSteps.js
+++ front/src/components/CheckoutSteps.js
@@ -4,44 +4,44 @@ import {LinkContainer} from 'react-router-bootstrap'
 
 const CheckoutSteps = ({step1, step2, step3, step4}) => {
   return (
-    <Nav className='justify-content-center mb-4'>
+    <Nav className='justify-content-center aligh-items-center mb-4 absolute-center'>
       <Nav.Item>
         {step1 ? (
           <LinkContainer to='/login'>
-            <Nav.Link>Sign In</Nav.Link>
+            <Nav.Link>Login</Nav.Link>
           </LinkContainer>
         ) : (
-         <Nav.Link disabled>Sign In</Nav.Link>
+         <Nav.Link disabled>Login</Nav.Link>
         )}
       </Nav.Item>
 
       <Nav.Item>
         {step2 ? (
           <LinkContainer to='/shipping'>
-            <Nav.Link>Shipping</Nav.Link>
+            <Nav.Link>Endereço</Nav.Link>
           </LinkContainer>
         ) : (
-         <Nav.Link disabled>Shipping</Nav.Link>
+         <Nav.Link disabled>Endereço</Nav.Link>
         )}
       </Nav.Item>
 
       <Nav.Item>
         {step3 ? (
           <LinkContainer to='/payment'>
-            <Nav.Link>Payment</Nav.Link>
+            <Nav.Link>Pagamento</Nav.Link>
           </LinkContainer>
         ) : (
-         <Nav.Link disabled>Payment</Nav.Link>
+         <Nav.Link disabled>Pagamento</Nav.Link>
         )}
       </Nav.Item>
 
       <Nav.Item>
         {step4 ? (
           <LinkContainer to='/placeorder'>
-            <Nav.Link>Placeorder</Nav.Link>
+            <Nav.Link>Revisar</Nav.Link>
           </LinkContainer>
         ) : (
-         <Nav.Link disabled>Placeorder</Nav.Link>
+         <Nav.Link disabled>Revisar</Nav.Link>
         )}
       </Nav.Item>
     </Nav>
