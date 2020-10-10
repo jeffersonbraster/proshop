dee6368d1a4acae2b1f3e3c8037978069eea6445
Jefferson <jeffersonbraster@gmail.com>
2020-10-10 13:05:47 -0300

    Show orders in profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,17 @@ import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
 
+
+
+ // @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id })
+  res.json(orders) 
+})
+
+
 // @desc Create new order
 // @route POST /api/orders
 // @access Private
@@ -70,4 +81,6 @@ const updateOrderToPaid = asyncHandler(async(req, res) => {
  })
 
 
-export {addOrderItems, getOrderById, updateOrderToPaid}
+
+
+export {addOrderItems, getOrderById, updateOrderToPaid, getMyOrders}
