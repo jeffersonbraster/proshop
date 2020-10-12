e546a7449961a592bb6af25da71d611a01f9a12f
Jefferson <jeffersonbraster@gmail.com>
2020-10-11 22:05:44 -0300

    Criando get user by id e update user endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -134,4 +134,46 @@ const deleteUser = asyncHandler(async(req, res) => {
   }
 })
 
-export {authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser}
+// @desc GET  user by id
+// @route GET /api/users/:id
+// @access Private/admin
+const getUserById = asyncHandler(async(req, res) => {    
+
+  const user = await User.findById(req.params.id).select('-password')
+  if(user) {
+    res.json(user)
+  }else {
+    res.status(404)
+    throw new Error('Usuário não existe')
+  }  
+})
+
+// @desc Update user 
+// @route PUT /api/users/:id
+// @access Private/admin
+const updateUser = asyncHandler(async(req, res) => {    
+
+  const user = await User.findById(req.params.id)
+
+  if(!user) {
+    res.status(404)
+    throw new Error('usuário não encontrado')
+  }
+
+  
+  user.name = req.body.name || user.name
+  user.email = req.body.email || user.email
+  user.isAdmin = req.body.isAdmin
+
+  const updateUser = await user.save()
+  
+  res.json({
+    _id: updateUser._id,
+    name: updateUser.name,
+    email: updateUser.email,
+    isAdmin: updateUser.isAdmin,
+  })
+  
+})
+
+export {authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser}
