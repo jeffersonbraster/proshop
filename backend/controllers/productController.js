7f942036ed8060380252476c434576b1d726645e
Jefferson <jeffersonbraster@gmail.com>
2020-10-13 19:29:18 -0300

    Create e update produtos endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -40,8 +40,57 @@ const deleteProduct = asyncHandler(async(req, res) => {
   }
 })
 
+// @desc create a product
+// @route post /api/products
+// @access Private/admin
+const createProduct = asyncHandler(async(req, res) => {
+  const product = new Product({
+    name: 'Nome do produto',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Marca do produto',
+    category: 'Categoria do produto',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Simples descrição'
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+// @desc update a product
+// @route put /api/products/:id
+// @access Private/admin
+const updateProduct = asyncHandler(async(req, res) => {
+  const {name, price, description, image, brand, category, countInStock} = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if(product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updateProduct = await product.save()
+    res.json(updateProduct)
+  }else {
+    res.status(404)
+    throw new Error('Produto não encontrado')
+  }
+
+  
+})
+
 export {
   getProducts,
   getProductById,
-  deleteProduct
+  deleteProduct,
+  createProduct,
+  updateProduct
 }
