05ca4a71128db4384cb6868a930f223b74d6b81b
Jefferson <jeffersonbraster@gmail.com>
2020-10-14 16:33:52 -0300

    Fazendo upload da imagem no frontend

--- front/src/screens/ProductEditScreen.js
+++ front/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import {Form, Button} from 'react-bootstrap'
@@ -20,6 +21,7 @@ const ProductEditScreen = ({match, history}) => {
   const [category, setCategory] = useState('')
   const [countInStock, setCountInStock] = useState(0)
   const [description, setDescription] = useState('')
+  const [uploading, setUploadin] = useState(false)
 
   const dispatch = useDispatch()
 
@@ -47,6 +49,29 @@ const ProductEditScreen = ({match, history}) => {
       }
     }  
   }, [dispatch, product, productId, history, successUpdate])
+
+  const uploadHandler = async (e) => {
+    const file = e.target.files[0]
+    const formData = new FormData()
+    formData.append('image', file)
+    setUploadin(true)
+
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+
+      const {data} = await axios.post('/api/upload', formData, config)
+
+      setImage(data)
+      setUploadin(false)
+    } catch (error) {
+      console.error(error)
+      setUploadin(false)
+    }
+  }
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -90,6 +115,9 @@ const ProductEditScreen = ({match, history}) => {
               <Form.Label>Imagem: </Form.Label>
               <Form.Control type="text" placeholder="Insira sua imagem" value={image} onChange={e => setImage(e.target.value)}>
               </Form.Control>
+              <Form.File id="image-file" label="Escolha o arquivo" custom onChange={uploadHandler}>
+                {uploading && (<Loader />)}
+              </Form.File>
             </Form.Group>
 
             <Form.Group controlId="brand">
